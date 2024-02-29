f5d58b027cd516736d8a0a7939b0243e87bd323b
GuidoLevy <guidoalevy@gmail.com>
2024-02-29 12:26:33 -0300

    starting with redux

--- labanda/src/Components/Header/header.jsx
+++ labanda/src/Components/Header/header.jsx
@@ -10,7 +10,7 @@ export default function Header (){
         <header>
                 <div class="caja">
                     <img src="https://www.cariverplate.com.ar/images/favicon-96x96.png" alt="logo Club Atletico River Plate"/>
-                    <a href="/"><h1>LA BANDA</h1></a>
+                    <a id="cabecera" href="/"><h1>LA BANDA</h1></a>
                 </div>
                 <nav>
                     <ul>
