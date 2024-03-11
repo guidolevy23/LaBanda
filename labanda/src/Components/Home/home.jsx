69d534e97d790837c5940bd62c4fa3efad477e91
GuidoLevy <guidoalevy@gmail.com>
2024-03-11 16:02:51 -0300

    starting creating the post images

--- labanda/src/Components/Home/home.jsx
+++ labanda/src/Components/Home/home.jsx
@@ -6,6 +6,7 @@ import Vidriera from "../Vidriera/vidriera";
 import Header from "../Header/header";
 import Newsletter from "../Newsletter/newsletter";
 import {useDispatch} from "react-redux"
+import Post from "./post";
 
 
 export default function Home(){
@@ -17,6 +18,7 @@ export default function Home(){
             <Vidriera {...name}/>
             <Newsletter/>
             <Matches/>
+            <Post/>
             <Footer/>
         </>
     )
---
+++ labanda/src/Components/Home/post.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Post(){
+    return(
+        <div className="post">
+            <h2>Subi tu foto en el monumental alentando a La Banda!!!</h2>
+            {/* Aca iria un carrusel de fotos que haya subidas */}
+            {/* Aca un Subi la tuya aca! con un formulario mini con un boton que despache la imagen y despache la accion de traer todas las imagenes para poder verlas */}
+            <form action=""></form>
+
+        </div>
+    )
+}
